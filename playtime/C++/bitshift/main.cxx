88e507e2c36905eb8a3b540098d29dfd1c9fb679
craig <craig@jailbox1.(none)>
2012-07-02 10:54:44 -0400

    Playing with shifts a bit to test some things.

---
+++ playtime/C++/bitshift/main.cxx
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+   typedef long long Handle;
+   std::cerr << "Size of long long is: " << sizeof(long long) << "\n";
+   std::cerr << "Size of long is: " << sizeof(long) << "\n";
+
+
+   Handle i;
+}
