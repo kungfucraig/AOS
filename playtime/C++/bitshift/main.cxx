8f542365e61d5444e4cdc55eaf95ee556ce523eb
craig <craig@jailbox1.(none)>
2012-07-03 15:51:25 -0400

    Playing with shifting of bits in isolated environment.

--- playtime/C++/bitshift/main.cxx
+++ playtime/C++/bitshift/main.cxx
@@ -2,10 +2,32 @@
 
 int main()
 {
-   typedef long long Handle;
-   std::cerr << "Size of long long is: " << sizeof(long long) << "\n";
-   std::cerr << "Size of long is: " << sizeof(long) << "\n";
+   typedef unsigned long long Handle;
+   typedef unsigned int uint;
+   const uint BITS_IN_BYTE=8;
+   const uint SHIFT_LENGTH = sizeof(uint)*BITS_IN_BYTE;
+
+   std::cerr << "Size of long long is: " << sizeof(Handle) << "\n";
+   std::cerr << "Size of long is: " << sizeof(uint) << "\n";
+
+   const uint top = 1;
+   const uint bottom = 100;
+   
+   Handle h=0;
+   h = h|top;
+   std::cerr << "After first or: " << h << "\n";
+   h<<=SHIFT_LENGTH;
+   std::cerr << "After first shift: " << h << "\n";
+   h=h|bottom;
+   std::cerr << "After second or: " << h << "\n";
 
 
-   Handle i;
+   uint topout = 0;
+   uint bottomout = 0;
+
+   bottomout = h;// we implicitly truncate top 32 bits
+   std::cerr << "Bottom is: " << bottomout << "\n";
+
+   topout = h>>SHIFT_LENGTH;
+   std::cerr << "Top is: " << topout << "\n";
 }
