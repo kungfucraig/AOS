c4cf0f84f525cb5fb3185adb936527c5a4bb83d6
Craig Wright <craig@artofsoftware.org>
2012-10-03 15:51:43 -0400

    Simple little test of partial template specialization.

---
+++ playtime/C++/partial-template-specialization/test.cxx
@@ -0,0 +1,47 @@
+#include <iostream>
+
+struct ReadWrite
+{
+      static void echo(){std::cerr << "ReadWrite.\n";}
+};
+
+struct ReadOnly
+{
+      static void echo(){std::cerr << "ReadOnly.\n";}
+};
+
+template<class ReturnType, typename AccessPolicy>
+class Functor
+{
+};
+
+template<typename AccessPolicy>
+class Functor<double, AccessPolicy>
+{
+   public:
+      void get(double& x){std::cerr << "Getting double.\n";  AccessPolicy::echo(); x=1.23;}
+};
+
+template<typename AccessPolicy>
+class Functor<int, AccessPolicy>
+{
+   public:
+      void get(int& x){std::cerr << "Getting int.\n"; AccessPolicy::echo(); x=123;}
+};
+
+
+int main()
+{
+   Functor<double, ReadOnly> f1;
+   Functor<double, ReadWrite> f2;
+   Functor<int, ReadOnly> f3;
+   Functor<int, ReadWrite> f4;
+
+   double x = 0;
+   f1.get(x); std::cerr << x << "\n";
+   f2.get(x); std::cerr << x << "\n";
+
+   int y;
+   f3.get(y); std::cerr << y << "\n";
+   f4.get(y); std::cerr << y << "\n";
+}
