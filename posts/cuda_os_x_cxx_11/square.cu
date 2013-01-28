efd261d843c05aad9fcbb1badc2a57593bee5580
Craig Wright <kungfucraig@gmail.com>
2013-01-28 16:16:22 -0500

    Updates to documentation.

--- posts/cuda_os_x_cxx_11/square.cu
+++ posts/cuda_os_x_cxx_11/square.cu
@@ -1,10 +1,27 @@
+// Copyright (c) 2013 Craig Wright (kungfucraig@gmail.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies
+// or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 #include <cuda_runtime.h>
-#include <stdio.h>
 
 //! Square the data.
-__global__ void
-square(float *data)
+__global__ void square(float *data)
 {
    const unsigned int tid = threadIdx.x;
    float d = data[tid];
@@ -12,8 +29,7 @@ square(float *data)
 }
 
 //! Provide a wrapper for the kernel.
-extern "C"
-cudaError_t runKernel(int len, float *dataD)
+extern "C" cudaError_t runKernel(int len, float *dataD)
 {
    dim3 grid(1, 1, 1);
    dim3 threads(len, 1, 1);
